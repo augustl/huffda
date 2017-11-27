8d96d002b3dd218ca07d772414d4c942bb14b0cd
August Lilleaas <august@augustl.com>
2017-11-27 23:32:32 +0100

    Add macro to wrap async go block tests

--- huffda-core/test/huffda/expectations_basics_test.cljs
+++ huffda-core/test/huffda/expectations_basics_test.cljs
@@ -1,16 +1,20 @@
 (ns huffda.expectations-basics-test
   (:require [clojure.test :refer [deftest testing is async]]
             [huffda.expectations :as expec]
-            [cljs.core.async :refer [chan <! >! put! close! alts! timeout promise-chan]])
+            [cljs.core.async :refer [chan <! >! put! close! alts! timeout promise-chan take!]])
   (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
 
-(deftest expectations-basics
-  (testing "should work"
+(defn test-async [name ch]
+  (testing name
     (async done
-      (go
-        (let [[db err] (<! (expec/create-memory-database))]
-          (<! (expec/add-expectation db {:key "my-expec-1"}))
-          (is (do (not (<! (expec/is-fulfilled db "my-expec-1")))))
-          (<! (expec/fulfill-expectation db "my-expec-1" true))
-          (is (do (<! (expec/is-fulfilled db "my-expec-1")))))
-        (done)))))
+      (take! ch (fn [_] (done))))))
+
+(deftest expectations-basics
+  (test-async
+    "should-work"
+    (go
+      (let [[db err] (<! (expec/create-memory-database))]
+        (<! (expec/add-expectation db {:key "my-expec-1"}))
+        (is (do (not (<! (expec/is-fulfilled db "my-expec-1")))))
+        (<! (expec/fulfill-expectation db "my-expec-1" true))
+        (is (do (<! (expec/is-fulfilled db "my-expec-1"))))))))
