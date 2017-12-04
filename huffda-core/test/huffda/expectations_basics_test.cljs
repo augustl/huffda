83f0cbc3aff55400e3783f391942f3948d3a6644
August Lilleaas <august@kodemaker.no>
2017-12-04 14:25:40 +0100

    Add test-async-with-db that creates and closes db per test

--- huffda-core/test/huffda/expectations_basics_test.cljs
+++ huffda-core/test/huffda/expectations_basics_test.cljs
@@ -1,22 +1,19 @@
 (ns huffda.expectations-basics-test
-  (:require [clojure.test :refer [deftest testing is async]]
+  (:require [cljs.test :refer [deftest testing is async]]
             [huffda.expectations :as expec]
-            [cljs.core.async :refer [chan <! >! put! close! alts! timeout promise-chan take!]])
-  (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
-
-(defn test-async [name ch]
-  (testing name
-    (async done
-      (take! ch (fn [_] (done))))))
+            [cljs.core.async :refer [chan <! >! put! close! alts! timeout promise-chan take!]]
+            [huffda.test-helper :refer [test-async]])
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]]
+                   [huffda.test-helper :refer [test-async-with-db]]))
 
 (deftest expectations-basics
-  (test-async
+  (test-async-with-db
     "should-work"
+    db
     (go
-      (let [[db err-db] (<! (expec/create-memory-database))]
-        (<! (expec/add-expectation db "my-expec-1" 123))
-        (let [[res err] (<! (expec/get-expectation db "my-expec-1"))]
-          (is (not (:is-fulfilled res))))
-        (<! (expec/fulfill-expectation db "my-expec-1" true))
-        (let [[res err] (<! (expec/get-expectation db "my-expec-1"))]
-          (is (:is-fulfilled res)))))))
+      (<! (expec/add-expectation db "my-expec-1" 123))
+      (let [[res err] (<! (expec/get-expectation db "my-expec-1"))]
+        (is (not (:is-fulfilled res))))
+      (<! (expec/fulfill-expectation db "my-expec-1" true))
+      (let [[res err] (<! (expec/get-expectation db "my-expec-1"))]
+        (is (:is-fulfilled res))))))
---
+++ huffda-core/test/huffda/test_helper.cljc
@@ -0,0 +1,20 @@
+(ns huffda.test-helper
+  (:require [cljs.test :refer [deftest testing is async]]))
+
+(defmacro test-async [name ch]
+  `(testing ~name
+     (async done#
+       (cljs.core.async/take! ~ch (fn [_#] (done#))))))
+
+(defmacro test-async-with-db [name db-sym & body]
+  `(testing ~name
+     (async done#
+       (cljs.core.async/take!
+         (huffda.expectations/create-memory-database)
+         (fn [[db# db-err#]]
+           (cljs.core.async/take!
+             (let [~db-sym db#]
+               (do ~@body))
+             (fn [_#]
+               (.close (:db db#))
+               (done#))))))))
